003cb3891c68a1895658907b847161de3cdf7531
yogisingh410 <y2singh.katheria@gmail.com>
2018-10-29 03:06:54 +0530

    Update SupervisedPrediction.py

--- AmericanExpressProblem2/SupervisedPrediction.py
+++ AmericanExpressProblem2/SupervisedPrediction.py
@@ -1,17 +1,5 @@
 # -*- coding: utf-8 -*-
 # Artificial Neural Network
-
-# Installing Theano
-# pip install --upgrade --no-deps git+git://github.com/Theano/Theano.git
-
-# Installing Tensorflow
-# pip install tensorflow
-
-# Installing Keras
-# pip install --upgrade keras
-
-# Part 1 - Data Preprocessing
-
 # Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,27 +11,12 @@ dataset_test = pd.read_csv('test.csv')
 X_train = dataset.iloc[0:, 1:55].values
 y_train = dataset.iloc[:, 55].values
 X_test =  dataset_test.iloc[:, 1:55].values
-# Encoding categorical data
-#from sklearn.preprocessing import LabelEncoder, OneHotEncoder
-#labelencoder_X_1 = LabelEncoder()
-#X[:, 1] = labelencoder_X_1.fit_transform(X[:, 1])
-#labelencoder_X_2 = LabelEncoder()
-#X[:, 2] = labelencoder_X_2.fit_transform(X[:, 2])
-#onehotencoder = OneHotEncoder(categorical_features = [1])
-#X = onehotencoder.fit_transform(X).toarray()
-#X = X[:, 1:]
-
-# Splitting the dataset into the Training set and Test set
-#from sklearn.model_selection import train_test_split
-#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
 # Feature Scaling
 from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
-
-# Part 2 - Now let's make the ANN!
 
 # Importing the Keras libraries and packages
 
